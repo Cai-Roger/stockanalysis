d3546ab55d89e8d0c3e68837a4de1780849f6d84
Cai-Roger <roger.cai@hand-global.com>
2026-04-09 00:30:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ try:
     import yfinance as yf
 except ImportError:
     os.system('pip install yfinance pandas matplotlib')
-import yfinance as yf
 import streamlit as st
 import yfinance as yf
 import pandas as pd
